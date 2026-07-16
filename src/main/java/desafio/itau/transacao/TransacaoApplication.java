cc2123ce6d48b2f2e6abec4dbe8380cc06b346ea
Pedro <pedrohenriquesb02@gmail.com>
2026-07-16 19:57:56 -0300

    feature(redis): Configurando o cache de operações utilizando o Redis

--- src/main/java/desafio/itau/transacao/TransacaoApplication.java
+++ src/main/java/desafio/itau/transacao/TransacaoApplication.java
@@ -2,8 +2,10 @@ package desafio.itau.transacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TransacaoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/desafio/itau/transacao/config/RedisConfiguration.java
@@ -0,0 +1,27 @@
+package desafio.itau.transacao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJacksonJsonRedisSerializer(new ObjectMapper())));
+
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
+    }
+}
